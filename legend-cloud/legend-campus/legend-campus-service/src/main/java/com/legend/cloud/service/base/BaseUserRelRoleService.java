341f357aa498f97a657c5efa41cf82a8eb6021f7
Hp <35661942+hupeiD@users.noreply.github.com>
2018-03-27 23:13:58 +0800

    新增base_user_rel_role，更改system_permission和system_menu

---
+++ legend-cloud/legend-campus/legend-campus-service/src/main/java/com/legend/cloud/service/base/BaseUserRelRoleService.java
@@ -0,0 +1,13 @@
+package com.legend.cloud.service.base;
+
+import com.legend.cloud.entity.base.BaseUserRelRole;
+import com.legend.module.core.service.core.LegendService;
+
+/**
+*
+ * @author Administrator
+ * @date 2018/3/8
+ */
+public interface BaseUserRelRoleService extends LegendService<BaseUserRelRole> {
+
+}
