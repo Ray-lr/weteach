938d25a7c8cc82ef1e24b583afa1e07e5b8ee732
Hp <35661942+hupeiD@users.noreply.github.com>
2018-03-29 21:51:30 +0800

    将Key更改为module

--- legend-cloud/legend-campus/legend-campus-web/src/main/java/com/legend/cloud/model/constant/attribute/Module.java
+++ legend-cloud/legend-campus/legend-campus-web/src/main/java/com/legend/cloud/model/constant/attribute/Module.java
@@ -4,7 +4,7 @@ package com.legend.cloud.model.constant.attribute;
  * @author Administrator
  * @date 2018/3/25
  */
-public class Key {
+public class Module {
 
     public static final String BASE = "base";
 
