119aa10f9da51cc022e26f1470d689498d0e205c
Hp <35661942+hupeiD@users.noreply.github.com>
2018-04-10 14:59:39 +0800

    vue的全局设置，包括变量，函数和过滤器等

--- legend-cloud/legend-campus/legend-campus-web/src/main/resources/static/js/common/vue/vue-global.js
+++ legend-cloud/legend-campus/legend-campus-web/src/main/resources/static/js/common/vue/vue-global.js
@@ -1,3 +1,18 @@
+/**
+ * 全局变量
+ */
+Vue.prototype.userInfo = null;
+
+/**
+ * 全局函数
+ */
+Vue.prototype.search = function (val) {
+    alert(val);
+};
+
+/**
+ * 全局过滤器
+ */
 Vue.filter('dateTime', function (value) {
     return new Date(value).Format('yyyy-MM-dd hh:mm:ss');
 });
