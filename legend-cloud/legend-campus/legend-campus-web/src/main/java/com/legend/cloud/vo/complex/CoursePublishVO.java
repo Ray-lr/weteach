3c68763967a064c19cfc506f32d8b86d36c74826
lirui1123211 <33196937+lirui1123211@users.noreply.github.com>
2018-04-11 16:07:54 +0800

    publishVO

---
+++ legend-cloud/legend-campus/legend-campus-web/src/main/java/com/legend/cloud/vo/complex/CoursePublishVO.java
@@ -0,0 +1,32 @@
+package com.legend.cloud.vo.complex;
+
+import com.legend.cloud.vo.base.BaseOrderVO;
+import com.legend.cloud.vo.campus.CampusCourseLimitVO;
+import com.legend.cloud.vo.campus.CampusCourseVO;
+import com.legend.cloud.vo.campus.CampusOrderVO;
+import lombok.Data;
+
+import javax.validation.Valid;
+
+@Data
+public class CoursePublishVO {
+    /**
+     * 发布课程提交的课程信息
+     */
+    @Valid
+    private CampusCourseVO course;
+    /**
+     * 发布课程提交的限制条件
+     */
+    @Valid
+    private CampusCourseLimitVO limit;
+    /**
+     * 基本订单表要先存订单基本信息
+     */
+    @Valid
+    private BaseOrderVO base;
+    /**
+     * 订单表存订单详细信息
+     */
+    private CampusOrderVO order;
+}
