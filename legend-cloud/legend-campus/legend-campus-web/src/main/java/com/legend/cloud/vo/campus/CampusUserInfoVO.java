a2d5f8667de105796d23396bead3413d23588195
Hp <35661942+hupeiD@users.noreply.github.com>
2018-04-11 15:40:43 +0800

    增加Username字段，并在转json时忽略某些字段

--- legend-cloud/legend-campus/legend-campus-web/src/main/java/com/legend/cloud/vo/campus/CampusUserInfoVO.java
+++ legend-cloud/legend-campus/legend-campus-web/src/main/java/com/legend/cloud/vo/campus/CampusUserInfoVO.java
@@ -1,5 +1,6 @@
 package com.legend.cloud.vo.campus;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.legend.cloud.entity.campus.CampusUserInfo;
 import com.legend.module.core.vo.AbstractVO;
 import lombok.Data;
@@ -7,6 +8,7 @@ import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.Null;
 import java.util.Date;
 
 
@@ -25,11 +27,18 @@ public class CampusUserInfoVO extends AbstractVO<CampusUserInfo> {
     /**
      * 用户id
      */
+    @Null
     private Integer id;
     /**
      * 用户基础id
      */
+    @Null
     private Integer baseUserId;
+    /**
+     * 用户名
+     */
+    @Null
+    private String username;
     /**
      * 用户实名
      */
@@ -41,27 +50,33 @@ public class CampusUserInfoVO extends AbstractVO<CampusUserInfo> {
     /**
      * 性别
      */
+    @JSONField(serialize = false)
     private String sex;
     /**
      * 出生年月
      */
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JSONField(serialize = false)
     private Date birthday;
     /**
      * 省/直辖市/自治区/特别行政区
      */
+    @JSONField(serialize = false)
     private Integer province;
     /**
      * 市/自治州
      */
+    @JSONField(serialize = false)
     private Integer city;
     /**
      * 县/区
      */
+    @JSONField(serialize = false)
     private Integer county;
     /**
      * 入学年份
      */
+    @JSONField(serialize = false)
     private String enrollmentYear;
     /**
      * 系别
@@ -78,14 +93,18 @@ public class CampusUserInfoVO extends AbstractVO<CampusUserInfo> {
     /**
      * 电话
      */
+    @JSONField(serialize = false)
+    @Null
     private String phone;
     /**
      * 邮箱
      */
+    @JSONField(serialize = false)
     private String email;
     /**
      * 联系方式（QQ,微信,等等）
      */
+    @JSONField(serialize = false)
     private String contact;
     /**
      * 个性签名
@@ -94,42 +113,54 @@ public class CampusUserInfoVO extends AbstractVO<CampusUserInfo> {
     /**
      * 积分量
      */
+    @JSONField(serialize = false)
     private Integer credits;
     /**
      * 积分信誉等级
      */
+    @JSONField(serialize = false)
     private String creditsLevel;
     /**
      * 参与课程数
      */
+    @JSONField(serialize = false)
     private Integer orderJoin;
     /**
      * 课程发布数
      */
+    @JSONField(serialize = false)
     private Integer orderPublish;
     /**
      * 课程完成数
      */
+    @JSONField(serialize = false)
     private Integer orderFinish;
     /**
      * 课程完成率
      */
+    @JSONField(serialize = false)
     private Double percentageComplete;
     /**
      * 创建时间
      */
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JSONField(serialize = false)
+    @Null
     private Date createTime;
     /**
      * 修改时间
      */
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JSONField(serialize = false)
+    @Null
     private Date updateTime;
     /**
      * 是否删除
      */
+    @JSONField(serialize = false)
+    @Null
     private Boolean isDeleted;
-    
+
     @Override
     public CampusUserInfo parseTo(String... ignoreProperties) {
         return super.parseTo(ignoreProperties);
