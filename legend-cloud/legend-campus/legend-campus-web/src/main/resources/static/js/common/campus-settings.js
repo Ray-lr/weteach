a68db7a4129563a31b99c7489c1ccdb6b2ff264e
Hp <35661942+hupeiD@users.noreply.github.com>
2018-04-11 16:53:26 +0800

    增加星级插件的设置

--- legend-cloud/legend-campus/legend-campus-web/src/main/resources/static/js/common/campus-settings.js
+++ legend-cloud/legend-campus/legend-campus-web/src/main/resources/static/js/common/campus-settings.js
@@ -1,18 +1,30 @@
 $(document).ready(function () {
+    /**
+     * bootstrap的相关设置
+     */
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
     });
 
+    /**
+     * messenger的相关设置
+     */
     $._messengerDefaults = {
         extraClasses: 'messenger-fixed messenger-theme-air messenger-on-center-sm'
     };
 
+    /**
+     * Pace的相关设置
+     */
     Pace.options = {
         ajax: true, // disabled
         document: false, // disabled
         eventLag: false // disabled
     };
 
+    /**
+     * dateTimePicker的相关设置
+     */
     $('.form_datetime').datetimepicker({
         orientation: "bottom auto",
         format: "yyyy-mm-dd hh:ii",
@@ -50,5 +62,20 @@ $(document).ready(function () {
         maxView: 1,
         clearBtn: 1,
         forceParse: 0
+    });
+
+    /**
+     * 星级插件设置
+     */
+    $(".star").raty({
+        score: function () {
+            return $(this).attr("data-num");
+        },
+        starOn: '/static/external/star-rating/image/star-on.png',
+        starOff: '/static/external/star-rating/image/star-off.png',
+        starHalf: '/static/external/star-rating/image/star-half.png',
+        readOnly: true,
+        halfShow: true,
+        size: 34,
     });
 });
