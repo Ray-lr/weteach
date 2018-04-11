91e4f399d0346cb5781bff93292430bf9380151f
Hp <35661942+hupeiD@users.noreply.github.com>
2018-04-11 15:38:58 +0800

    CampusUserInfoController.update() 更改代码逻辑

--- legend-cloud/legend-campus/legend-campus-web/src/main/java/com/legend/cloud/controller/campus/CampusUserInfoController.java
+++ legend-cloud/legend-campus/legend-campus-web/src/main/java/com/legend/cloud/controller/campus/CampusUserInfoController.java
@@ -5,10 +5,8 @@ import com.alibaba.fastjson.JSON;
 import com.legend.cloud.controller.CampusController;
 import com.legend.cloud.entity.base.BaseUser;
 import com.legend.cloud.entity.campus.CampusUserInfo;
-import com.legend.cloud.service.base.BaseAreasService;
 import com.legend.cloud.service.base.BaseUserService;
 import com.legend.cloud.service.campus.CampusUserInfoService;
-import com.legend.cloud.vo.base.BaseUserVO;
 import com.legend.cloud.vo.campus.CampusUserInfoVO;
 import com.legend.module.core.model.contant.arribute.Column;
 import com.legend.module.core.model.contant.arribute.Key;
@@ -18,14 +16,16 @@ import com.legend.module.core.model.group.option.AddGroup;
 import com.legend.module.core.model.group.option.UpdateGroup;
 import com.legend.module.core.model.json.result.Ajax;
 import com.legend.module.core.model.json.result.AjaxValidate;
-import com.legend.module.core.utils.HttpSessionUtils;
 import com.legend.module.core.utils.PageUtils;
 import com.legend.module.core.utils.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,12 +37,13 @@ import java.util.stream.Collectors;
 @RequestMapping("/campus/userInfo")
 public class CampusUserInfoController extends CampusController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CampusUserInfoController.class);
+
     @Resource
     private CampusUserInfoService campusUserInfoService;
+
     @Resource
     private BaseUserService baseUserService;
-    @Resource
-    private BaseAreasService baseAreasService;
 
     @GetMapping("/list")
     // @RequiresPermissions("campus:userInfo:list")
@@ -97,21 +98,27 @@ public class CampusUserInfoController extends CampusController {
             if (bindingResult.hasErrors()) {
                 return AjaxValidate.processBindingResult(bindingResult);
             }
-            BaseUserVO b = JSON.parseObject((String) HttpSessionUtils.getAttribute("currentUser"), BaseUserVO.class);
-            b.setNickName(campusUserInfoVO.getNickname());
-            b.setPhone(campusUserInfoVO.getPhone());
-            BaseUser baseUser = b.parseTo();
-            int updateResult = baseUserService.updateById(baseUser);
-            updateResult += campusUserInfoService.updateById(campusUserInfoVO.parseTo());
-            return updateResult == 2 ? Ajax.success(AjaxMessage.UPDATE_SUCCESS) : Ajax.error(AjaxMessage.UPDATE_FAILURE, AjaxCode
-                    .UPDATE_FAILURE);
+            CampusUserInfoVO currentUser = JSON.parseObject(String.valueOf(getCurrentUser()), CampusUserInfoVO.class);
+            campusUserInfoVO.setId(currentUser.getId());
+            campusUserInfoVO.setUpdateTime(new Date());
+            if (campusUserInfoService.updateById(campusUserInfoVO.parseTo()) <= 0) {
+                Ajax.error(AjaxMessage.UPDATE_FAILURE, AjaxCode.UPDATE_FAILURE);
+            }
+            // 重新查询并设置进session中
+            BaseUser baseUser = baseUserService.getById(currentUser.getBaseUserId());
+            CampusUserInfo campusUserInfo = campusUserInfoService.getById(currentUser.getId());
+            currentUser.parseFrom(campusUserInfo);
+            currentUser.setUsername(baseUser.getUsername());
+            setCurrentUser(JSON.toJSONString(currentUser));
+            LOGGER.info(String.valueOf(getCurrentUser()));
+            return Ajax.success(AjaxMessage.UPDATE_SUCCESS);
         } catch (Exception e) {
             e.printStackTrace();
             return Ajax.error(AjaxMessage.SERVER_ERROR, AjaxCode.SERVER_ERROR);
         }
     }
 
-    @PostMapping("/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     // @RequiresPermissions("campus:userInfo:delete")
     public Ajax delete(@PathVariable Integer id) {
         try {
