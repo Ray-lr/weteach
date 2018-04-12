84aec74127f227515789d28d263b21bdee7ab792
Hp <35661942+hupeiD@users.noreply.github.com>
2018-04-12 20:03:57 +0800

    更改用户类型

---
+++ legend-cloud/legend-campus/legend-campus-web/src/main/java/com/legend/cloud/model/constant/attribute/Host.java
@@ -0,0 +1,16 @@
+package com.legend.cloud.model.constant.attribute;
+
+/**
+ * @author Administrator
+ * @date 2018/4/12
+ */
+public class Host {
+    /**
+     * 管理员用户
+     */
+    public static final String ADMIN = "admin";
+    /**
+     * 普通用户
+     */
+    public static final String USER = "user";
+}
